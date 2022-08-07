100a0f46a83273584fe998c1510d2e27f832cf55
Иван Голенко <igolivanenko1996@gmail.com>
2022-08-07 22:49:07 +0300

    заливаю основу

---
+++ api_yamdb/api/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from users.models import User
+from reviews.models import Title, Category, Genre
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'year', 'category']
+
+
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(User)
