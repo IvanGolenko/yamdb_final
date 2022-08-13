00238c161c170bbb61f427badf33a679682552d4
Иван Голенко <igolivanenko1996@gmail.com>
2022-08-13 13:30:08 +0300

    i hate pep8

--- api_yamdb/api/admin.py
+++ api_yamdb/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from reviews.models import Category, Title
+from reviews.models import Genre
 from users.models import User
-from reviews.models import Category, Title, Genre
 
 
 class GenreAdmin(admin.ModelAdmin):
